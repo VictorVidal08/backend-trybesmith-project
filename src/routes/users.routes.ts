4f816dea54f274c17e641c0275535c3de8c4e7de
VictorVidal08 <victorvidal08@hotmail.com>
2022-09-14 22:20:53 -0300

    feat: creating users validations

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users';
+import UserValidation from '../middlewares/userValidation';
 
 const router = Router();
 
+const userValidation = new UserValidation();
+
 const usersController = new UsersController();
 
-router.post('/users', usersController.create);
+router.post(
+  '/users',
+  userValidation.classeValidation,
+  userValidation.levelValidation,
+  userValidation.passwordvalidation,
+  userValidation.usernameValidation,
+  usersController.create,
+);
 
 export default router;
