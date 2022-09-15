668c66698e1f4cce2935f665323fd117004e4ea2
VictorVidal08 <victorvidal08@hotmail.com>
2022-09-14 22:06:06 -0300

    feat: crating /products validations

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products';
+import ProductValidation from '../middlewares/productValidation';
 
 const router = Router();
 
+const productValidation = new ProductValidation();
+
 const productsController = new ProductsController();
 
-router.get('/products', productsController.getAll);
+router.get(
+  '/products',
+  productValidation.nameValidation,
+  productValidation.amountValidation,
+  productsController.getAll,
+);
 
-router.post('/products', productsController.create);
+router.post(
+  '/products',
+  productValidation.nameValidation,
+  productValidation.amountValidation,
+  productsController.create,
+);
 
 export default router;
